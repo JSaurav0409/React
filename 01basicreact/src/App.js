ae9136bae026fa6406545c7c28c0cc9969c6c00d
JSaurav0409 <sauravjha0409@gmail.com>
2024-10-03 15:37:07 +0530

    created first react app using npx and npm + bundler ( vite )

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>There are two ways to create a React application:</p>
+      <ol>
+        <li>Using <code>npx</code>, which consumes more resources.</li>
+        <li>Using <code>npm</code>, which consumes fewer resources.</li>
+      </ol>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>React with Vite | SJ</h1>
+      <p>
+        Vite is a next-generation frontend build tool that is significantly faster than traditional bundlers like Webpack. It provides instant server startup and lightning-fast hot module replacement (HMR), making development more efficient and enjoyable.
+      </p>
+      <h2>Briefly</h2>
+      <p>
+        Vite is fast, lightweight, and simplifies the development process with quick builds and real-time updates.
+      </p>
+    </>
+  );
+}
+
+export default App;
